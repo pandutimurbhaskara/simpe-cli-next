5a11e6cbf06ec20360467437808814d643638bde
pandutimurbhaskara <pandu.timur.bhaskara@maill.ugm.ac.id>
2022-05-12 16:27:51 +0700

    [FEATURE] Add folder sugestion search feature while entering foldrName

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ const {nextBoilerList} = require("./template/next_boiler");
 const fs = require("fs");
 const path = require("path");
 const {reactBoilerList} = require("./template/react_boiler");
+inquirer.registerPrompt('fuzzypath', require('inquirer-fuzzy-path'))
 
 questions = [{
     type: 'list',
@@ -25,11 +26,32 @@ questions = [{
     message: 'Choose next template',
     when: (answers) => answers.categoriesType === globalCategory.NEXT_BOILER
 }, {
-    type: 'input',
+    type: 'fuzzypath',
     name: 'directory',
-    message: 'Choose direcrtory',
-    when: (answers) => answers.template != null
-},{
+    excludePath: nodePath => nodePath.startsWith('node_modules'),
+    // excludePath :: (String) -> Bool
+    // excludePath to exclude some paths from the file-system scan
+    excludeFilter: nodePath => nodePath == '.',
+    // excludeFilter :: (String) -> Bool
+    // excludeFilter to exclude some paths from the final list, e.g. '.'
+    itemType: 'any',
+    // itemType :: 'any' | 'directory' | 'file'
+    // specify the type of nodes to display
+    // default value: 'any'
+    // example: itemType: 'file' - hides directories from the item list
+    rootPath: '.',
+    // rootPath :: String
+    // Root search directory
+    message: 'Select a target directory for your component:',
+    default: './',
+    suggestOnly: false,
+    // suggestOnly :: Bool
+    // Restrict prompt answer to available choices or use them as suggestions
+    depthLimit: 5,
+    // depthLimit :: integer >= 0
+    // Limit the depth of sub-folders to scan
+    // Defaults to infinite depth if undefined
+}, {
     type: 'input',
     name: 'fileName',
     message: 'Choose file name',
--- testOkee.js
+++
@@ -1,6 +0,0 @@
-
-export async function getStaticProps(context) {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
